eba3fc71d4e857132acb9bfb5026ae6da904f0ac
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-02-07 00:23:20 -0500

    created skeleton of main pages

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('/static', {extensions: ['html']}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+app.use((req, res) => {
+    // secure the backend code so it can't be accessed by the frontend
+    if (req.url === '/server.js') {
+        res.redirect('/');
+    } else if (req.url.substr(-5) !== '.html') {
+        res.sendFile(__dirname + '/static/' + req.url + '.html');
+    } else {
+        res.redirect(req.url.slice(0, -5));
+    }
+});
+
+server.listen(port, () => {
+    console.log(`listening at port=${port}`);
+});
