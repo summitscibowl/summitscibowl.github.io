e689c5c8fe5ceb82f2b2583ed7d2ab84ba04aecb
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-02-12 20:55:59 -0500

    fixed redirect

--- static/script.js
+++ static/script.js
@@ -1,3 +1,3 @@
-if (document.URL.substring(0, 36) === 'https://summitscibowl.herokuapp.com/') {
+if (document.URL.substring(0, 35) === 'https://summitscibowl.herokuapp.com') {
     window.location.href = 'http://www.summitscibowl.com' + document.URL.substring(36);
 }
