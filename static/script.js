960fdebfc073f8e71a8c9757e8009671b869bca7
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-03-08 05:35:00 -0500

    http to https redirect

--- static/script.js
+++ static/script.js
@@ -5,3 +5,7 @@ if (document.URL.substring(0, 35) === 'https://summitscibowl.herokuapp.com') {
 if (document.URL.substring(0, 39) === 'https://www.summitscibowl.herokuapp.com') {
     window.location.href = 'https://www.summitscibowl.com' + document.URL.substring(39);
 }
+
+if (document.URL.substring(0, 7) === 'http://') {
+    window.location.href = 'https://www.summitscibowl.com' + document.URL.substring(7);
+}
