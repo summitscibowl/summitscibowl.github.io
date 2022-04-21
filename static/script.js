4747dc676a351287bdad385a240b4563b08deba3
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-04-21 01:14:43 -0400

    fixed http/https

--- static/script.js
+++ static/script.js
@@ -6,6 +6,6 @@ if (document.URL.substring(0, 39) === 'https://www.summitscibowl.herokuapp.com')
     window.location.href = 'https://www.summitscibowl.com' + document.URL.substring(39);
 }
 
-// if (document.URL.substring(0, 7) === 'http://') {
-//     window.location.href = 'https://' + document.URL.substring(7);
-// }
+if (document.URL.substring(0, 7) === 'http://') {
+    window.location.href = 'https://' + document.URL.substring(7);
+}
